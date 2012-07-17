8c059fe6a53daeeb2c8cad349be3ff2ef52812e7
Eumir Gaspar <eumir@aelogica.com>
2012-07-17 16:22:35 +0800

    changed solution to just use same color as background

--- javascripts/exercises.js
+++ javascripts/exercises.js
@@ -1,14 +1,3 @@
 $(function(){
   $("pre").snippet("ruby", {style: "acid", clipboard: "/javascripts/ZeroClipboard.swf"});
-
-  $(".solution")
-    .hide()
-    .hover(
-      function () {
-        $(this).show();
-      },
-      function () {
-        $(this).hide();
-      }
-    );
 })
