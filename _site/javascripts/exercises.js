a62e2c61f3962854464460a9d8b9f7b3805c81b4
Eumir Gaspar <eumir@aelogica.com>
2012-07-17 16:18:41 +0800

    added hover js for solutions

--- _site/javascripts/exercises.js
+++ _site/javascripts/exercises.js
@@ -1,3 +1,12 @@
 $(function(){
-  $("pre").snippet("ruby", {style: "acid", clipboard: "/javascripts/ZeroClipboard.swf"})
+  $("pre").snippet("ruby", {style: "acid", clipboard: "/javascripts/ZeroClipboard.swf"});
+
+  $(".solution").hover(
+    function () {
+      $(this).show();
+    },
+    function () {
+      $(this).hide();
+    }
+  );
 })
--- javascripts/exercises.js
+++ javascripts/exercises.js
@@ -1,3 +1,14 @@
 $(function(){
-  $("pre").snippet("ruby", {style: "acid", clipboard: "/javascripts/ZeroClipboard.swf"})
+  $("pre").snippet("ruby", {style: "acid", clipboard: "/javascripts/ZeroClipboard.swf"});
+
+  $(".solution")
+    .hide()
+    .hover(
+      function () {
+        $(this).show();
+      },
+      function () {
+        $(this).hide();
+      }
+    );
 })
